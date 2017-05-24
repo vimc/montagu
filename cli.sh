821933b50e5de60fe49704fee9499182f47e2670
Martin Eden <martin.s.eden@gmail.com>
2017-05-24 10:47:35 +0100

    Make this a snapshot of known versions, rather than tracking master

--- cli.sh
+++ cli.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker run -ti \
     --network montagu_default \
-    montagu.dide.ic.ac.uk:5000/montagu-cli:master "$@"
+    montagu.dide.ic.ac.uk:5000/montagu-cli:0f59cdb "$@"
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-export MONTAGU_API_VERSION=master
-export MONTAGU_DB_VERSION=master
-export MONTAGU_CONTRIB_VERSION=master
+export MONTAGU_API_VERSION=0f59cdb
+export MONTAGU_DB_VERSION=d5ab6ad
+export MONTAGU_CONTRIB_VERSION=28ace6c
 docker-compose pull
 docker-compose up \
     --project-name montagu \
