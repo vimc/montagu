5ae11e0a12fc1bff7f29b099b8ff21c2717f7bb5
Martin Eden <martin.s.eden@gmail.com>
2017-05-24 10:45:39 +0100

    Add CLI

---
+++ cli.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+docker run -ti \
+    --network montagu_default \
+    montagu.dide.ic.ac.uk:5000/montagu-cli:master "$@"
--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ export MONTAGU_API_VERSION=master
 export MONTAGU_DB_VERSION=master
 export MONTAGU_CONTRIB_VERSION=master
 docker-compose pull
-docker-compose up --abort-on-container-exit
+docker-compose up \
+    --project-name montagu \
+    --abort-on-container-exit
