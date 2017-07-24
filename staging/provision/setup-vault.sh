bb635e711142f19366010fbb0501d2e7ee96a3a0
Martin Eden <martin.s.eden@gmail.com>
2017-07-24 15:58:21 +0100

    Stage VM: Give every user permission to run Vault

--- staging/provision/setup-vault.sh
+++ staging/provision/setup-vault.sh
@@ -10,6 +10,6 @@ else
 	vault_zip=vault_0.7.3_linux_amd64.zip
 	wget https://releases.hashicorp.com/vault/0.7.3/$vault_zip
 	unzip $vault_zip
-	chmod 744 vault
+	chmod 755 vault
 	sudo cp vault /usr/bin/vault
 fi
