e8109338a7b03ea8d00eee675bf79c2a9cd2a377
Martin Eden <martin.s.eden@gmail.com>
2017-07-24 15:55:40 +0100

    Improve Vault provisioning script

--- staging/provision/setup-vault.sh
+++ staging/provision/setup-vault.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 
-vault_zip=vault_0.7.3_linux_amd64.zip
-wget https://releases.hashicorp.com/vault/0.7.3/$vault_zip
-unzip $vault_zip
-chmod 744 vault
-cp vault /usr/bin/vault
+if which -a vault > /dev/null; then
+    echo "vault is already installed"
+else
+	echo "installing vault"
+	sudo apt-get update
+    sudo apt-get install -y unzip
+	vault_zip=vault_0.7.3_linux_amd64.zip
+	wget https://releases.hashicorp.com/vault/0.7.3/$vault_zip
+	unzip $vault_zip
+	chmod 744 vault
+	sudo cp vault /usr/bin/vault
+fi
