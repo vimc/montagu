74a3acc4bc2c884176be1d0bf77308f048db6927
Martin Eden <martin.s.eden@gmail.com>
2017-08-31 14:07:35 +0100

    Prevent tests from being run by accident.

--- src/test.py
+++ src/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from subprocess import run
 
+import sys
+
 import versions
 from docker_helpers import get_image_name
 
@@ -20,9 +22,14 @@ def api_blackbox_tests():
             "docker", "run", "--network", "montagu_default",
             "-v", "montagu_emails:/tmp/montagu_emails",
             image
-        ])
+        ], check=True)
 
     run_in_teamcity_block("api_blackbox_tests", work)
 
 if __name__ == "__main__":
-    api_blackbox_tests()
+    if len(sys.argv) > 1 and sys.argv[1] == "--run-tests":
+        api_blackbox_tests()
+    else:
+        print("Warning - these tests should not be run in a real environment. They will destroy or change data.")
+        print("To run the tests, run ./tests.py --run-tests")
+        exit(-1)
