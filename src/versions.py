797692ed2b97f6fdf6fdef2831d4b7ff281fa2fb
Alex Hill <alex.hill@gmail.com>
2017-09-27 15:31:05 +0100

    updated contrib to version with correct template names

--- src/versions.py
+++ src/versions.py
@@ -1,7 +1,7 @@
 api = "dac8181"
 reporting_api = "4264fea"
 db = "f988668"
-contrib_portal = "c84d844"
+contrib_portal = "34c9d22"
 admin_portal = "c84d844"
 report_portal = "c84d844"
 cert_tool = "ae10ceb"
