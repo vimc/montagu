ddee8e806ffa4db6bc2a319aa94c1ca97f82316d
Alex Hill <alex.hill@gmail.com>
2017-09-30 00:18:45 +0100

    update contrib to include new templates

--- src/versions.py
+++ src/versions.py
@@ -1,7 +1,7 @@
 api = "dac8181"
 reporting_api = "4264fea"
 db = "f988668"
-contrib_portal = "dd03f4d"
+contrib_portal = "93d044d"
 admin_portal = "c84d844"
 report_portal = "c84d844"
 cert_tool = "ae10ceb"
