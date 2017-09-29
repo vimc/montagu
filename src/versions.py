d193da3edf418292a9b81a9c36185eb28ba49509
Alex Hill <alex.hill@gmail.com>
2017-09-29 16:40:03 +0100

    update contrib

--- src/versions.py
+++ src/versions.py
@@ -1,7 +1,7 @@
 api = "dac8181"
 reporting_api = "4264fea"
 db = "f988668"
-contrib_portal = "34c9d22"
+contrib_portal = "5f28ce3"
 admin_portal = "c84d844"
 report_portal = "c84d844"
 cert_tool = "ae10ceb"
