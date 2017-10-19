7b3dfe30d3501ed96d096f483e305da8933d756a
Martin Eden <martin.s.eden@gmail.com>
2017-10-19 17:07:30 +0100

    Run webapp integration tests.

--- src/test.py
+++ src/test.py
@@ -28,9 +28,25 @@ def api_blackbox_tests():
 
     run_in_teamcity_block("api_blackbox_tests", work)
 
+
+def webapp_integration_tests():
+    def work():
+        image = get_image_name("montagu-portal-integration-tests", "local")
+        run([
+            "docker", "run",
+            "--rm",
+            "--network", "montagu_default",
+            "-v", "/var/run/docker.sock:/var/run/docker.sock",
+            image
+        ], check=True)
+
+    run_in_teamcity_block("webapp_integration_tests", work)
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "--run-tests":
         api_blackbox_tests()
+        webapp_integration_tests()
     else:
         print("Warning - these tests should not be run in a real environment. They will destroy or change data.")
         print("To run the tests, run ./tests.py --run-tests")
