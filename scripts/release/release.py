6841ad2c302d9efd47accc26f384061d2dc9e1b2
Martin Eden <martin.s.eden@gmail.com>
2017-10-26 15:10:46 +0100

    Check tag format.

--- scripts/release/release.py
+++ scripts/release/release.py
@@ -29,6 +29,18 @@ def push():
         run("git push --tags")
 
 
+def get_new_tag():
+    new_tag = "v" + input("What should the new release tag be? v")
+    if new_tag <= latest_tag:
+        template = "Error: {new_tag} is not after {latest_tag}"
+        print(template.format(new_tag=new_tag, latest_tag=latest_tag))
+        exit(-1)
+    if not release_tag_pattern.match(new_tag):
+        print("Error: tag does not correspond to regex")
+        exit(-1)
+    return new_tag
+
+
 if __name__ == "__main__":
     if False:  # not git_is_clean():
         print("Git status reports as not clean; aborting release")
@@ -43,10 +55,8 @@ if __name__ == "__main__":
         print(" ".join(diff))
         print("")
 
-        new_tag = "v" + input("What should the new release tag be? v")
-        if new_tag < latest_tag:
-            template = "{new_tag} is not after {latest_tag}"
-            print(template.format(new_tag=new_tag, latest_tag=latest_tag))
+        new_tag = get_new_tag()
+
         print("Tagging and pushing...")
         tag(new_tag, diff)
         push()
