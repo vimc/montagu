830203c27ecb64b6a4c9973a985a47de5e7ef538
Rich <r.fitzjohn@imperial.ac.uk>
2017-11-07 09:56:15 +0000

    Process for migration

--- scripts/generate_passwords.sh
+++ scripts/generate_passwords.sh
@@ -7,7 +7,11 @@ MACHINES="production science"
 for M in $MACHINES; do
     for U in $USERS; do
         KEY="secret/database/$M/users/$U"
+        # When closing out VIMC-587, uncomment the next line, delete
+        # the following, and rerun the script
+        # PW=$(pwgen -s -y 80 1)
+        PW=$(vault read -field=password "secret/database/users/$U")
         echo "Setting password $KEY"
-        echo vault write "$KEY" password=$(pwgen -n1 80)
+        vault write "$KEY" password="$PW"
     done
 done
