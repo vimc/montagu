27316b1a4325ef410f75e4db0cddb3a7ed3e3bcf
Martin Eden <martin.s.eden@gmail.com>
2017-11-10 13:47:23 +0000

    Simplify update script as-per Rich's suggestion

--- src/update_versions_to_latest_master.py
+++ src/update_versions_to_latest_master.py
@@ -7,12 +7,6 @@ from subprocess import run
 import versions
 
 if __name__ == "__main__":
-    root = "submodules"
-    for submodule in os.listdir(root):
-        full_path = join(root, submodule)
-        print(submodule + ": ", end="", flush=True)
-        run(["git", "checkout", "master", "--quiet"], cwd=full_path, check=True)
-        run(["git", "pull"], cwd=full_path, check=True)
-
+    run(["git", "submodule", "update", "--recursive", "--remote"])
     print("\nNew versions:")
     pprint.pprint(versions.as_dict())
