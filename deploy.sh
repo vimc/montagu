9a7247e1dad9e239a210bd7f15fb57072acaef14
Martin Eden <martin.s.eden@gmail.com>
2017-11-22 13:28:28 +0000

    Allow one button deploy of a tag

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 set -e
-pip3 install -r src/requirements.txt
+git fetch --all
+latest=$(git tag -l 'v*' | sort | tail -n 1)
+echo -n "Which tag do you want to deploy? [$latest] "
+read version
+version=${version:-$latest}
+
+echo "Checking out $version"
+git checkout $version
+git submodule update
+
+pip3 install --quiet -r src/requirements.txt
 python3 ./src/deploy.py
