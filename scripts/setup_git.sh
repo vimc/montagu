bdebf956b9c1a86b9c799338f89ebf0e9af78127
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-11-28 14:11:31 +0000

    Be more verbose

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -e
 SCRIPT=$0
 MONTAGU_PATH=$(git -C $(dirname $SCRIPT) rev-parse --show-toplevel)
 
 ## NOTE: this is *not* Rich's email but one associated with our robot
 ## account https://github.com/vimc-robot
+echo "Setting git username and password for $MONTAGU_PATH"
 git -C $MONTAGU_PATH config user.email "rich.fitzjohn+vimc@gmail.com"
 git -C $MONTAGU_PATH config user.name "vimc-robot"
+echo "Done"
