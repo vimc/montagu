331519c3b640271372627ab15354f6d11978c46f
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-12-05 11:06:05 +0000

    Harmonise restore proceedure between staging and restore

--- staging/shared/restore/restore.sh
+++ staging/shared/restore/restore.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 set -ex
+
+NAME=$1
+if [ ! "$#" -eq 1 ]; then
+    echo "Expected one argument"
+    exit 1
+fi
+
 if [ ! -f /vagrant/vault_config ]; then
     echo "Run restore-prepare.sh on host"
     exit 1
 fi
 
+MONTAGU_PATH=~/montagu
+
+MONTAGU_SETTINGS="/vagrant/restore/$NAME/montagu-deploy.json"
+MONTAGU_VERSION="/vagrant/restore/$NAME/version"
+
+if [ ! -f $MONTAGU_SETTINGS ]; then
+    echo "Settings not found at $MONTAGU_SETTINGS"
+    exit 1
+fi
+
+if [ ! -f $MONTAGU_VERSION ]; then
+    echo "Version not found at $MONTAGU_VERSION"
+    exit 1
+fi
+
+MONTAGU_VERSION_STRING=$(cat $MONTAGU_VERSION)
+
 . /vagrant/vault_config
-export VAULT_ADDR
-export VAULT_AUTH_GITHUB_TOKEN
+export VAULT_ADDR VAULT_AUTH_GITHUB_TOKEN
 
 vault auth -method=github
 vault read -field=password /secret/registry/vimc | \
@@ -15,16 +38,15 @@ vault read -field=password /secret/registry/vimc | \
 vault read -field=password /secret/registry/vimc | \
     sudo -H docker login -u vimc --password-stdin docker.montagu.dide.ic.ac.uk:5000
 
-MONTAGU_PATH=montagu
-
 if [ -f $MONTAGU_PATH ]; then
     (cd $MONTAGU_PATH && ./src/stop.py)
     rm -rf $MONTAGU_PATH
 fi
 
 git clone --recursive https://github.com/vimc/montagu $MONTAGU_PATH
+git -C $MONTAGU_PATH checkout $MONTAGU_VERSION_STRING
 
-cp /vagrant/restore/settings.json $MONTAGU_PATH/src/montagu-deploy.json
+cp $MONTAGU_SETTINGS $MONTAGU_PATH/src/montagu-deploy.json
 
 sudo mkdir -p /etc/montagu/backup
 pip3 install --user -r $MONTAGU_PATH/src/requirements.txt
