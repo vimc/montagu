ca927e33eb199c8db61c7d55ccdc2fa41cea7adc
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-12-14 11:48:35 +0000

    Fix website generation script

--- src/vimc_website.py
+++ src/vimc_website.py
@@ -5,18 +5,29 @@ import docker
 from docker_helpers import montagu_registry
 
 def build_vimc_website(ref="origin/i1205"):
-    volume_name = "montagu_website_volume"
+    volume_name = "montagu_vimc_website_volume"
     d = docker.client.from_env()
     img_name = "{}/vimc-website-builder:i1205".format(montagu_registry)
+    print("Pulling website builder image")
     img = d.images.pull(img_name)
     volumes = {
         volume_name: {"bind": "/srv/jekyll", "mode": "rw"}
     }
-    try:
-        res = d.containers.run(img, ["build-site.sh", ref],
-                               volumes = volumes, remove = True, tty = True)
-    except docker.errors.ContainerError as e:
-        res = e
+    # Note: using create/start/wait rather than run because otherwise
+    # it's really hard to get the logs on failed build (the python
+    # docker package returns only stderr on failure and the error is
+    # in stdout)
+    container = d.containers.create(img, ["build-site.sh", ref],
+                                    volumes = volumes, tty = True)
+    print("Building VIMC website")
+    container.start()
+    if container.wait() != 0:
+        print("...failed")
+        print(container.logs().decode("utf-8"))
+        container.remove()
+        raise Exception("Failed to build website")
+    container.remove()
+    print("...success")
 
 if __name__ == "__main__":
     build_vimc_website()
