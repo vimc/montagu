73a1f67a1df6a98d6ec5a57c8bf74a6a66576da7
Martin Eden <martin.s.eden@gmail.com>
2018-02-01 12:42:21 +0000

    Correct option

--- teamcity.sh
+++ teamcity.sh
@@ -3,4 +3,4 @@ set -ex
 cp teamcity-settings.json src/montagu-deploy.json
 pip3 install --quiet -r src/requirements.txt
 ./src/deploy.py
-./src/test.py $@ --simulate-reboot
+./src/test.py $@ --simulate-restart
