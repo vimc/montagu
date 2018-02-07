9f86008d783fb7ed95a38b8b59384884aac472aa
Alex Hill <alex.hill@gmail.com>
2018-02-07 16:36:30 +0000

    keep code DRY

--- src/service_config/contrib_portal_config.py
+++ src/service_config/contrib_portal_config.py
@@ -3,36 +3,26 @@ from os.path import join
 
 
 def configure_contrib_portal(service):
-    template_report_paths = get_template_report_versions()
-    guidance_paths = get_guidance_report_versions()
+    template_report_paths = get_report_versions("template_report_versions")
+    guidance_paths = get_report_versions("guidance_report_versions")
     print("Configuring contrib portal")
     for p in template_report_paths:
         if len(p) > 0:
             path_to_templates = join("archive", p, "*.csv")
-            add_templates_to_contrib_portal(service, path_to_templates)
+            add_reports_to_contrib_portal(service, path_to_templates, "templates")
     for p in guidance_paths:
         if len(p) > 0:
             path_to_artefacts = join("archive", p, "*.html")
-            add_guidance_to_contrib_portal(service, path_to_artefacts)
+            add_reports_to_contrib_portal(service, path_to_artefacts, "guidance")
 
 
-def add_templates_to_contrib_portal(service, path_to_templates):
-    print("- Copying burden estimate templates from orderly to contrib portal")
-    copy_between_volumes(service.volume_name("orderly"), service.volume_name("templates"), path_to_templates)
+def add_reports_to_contrib_portal(service, path_to_reports, volume_name):
+    print("- Copying {} reports from orderly to contrib portal".format(volume_name))
+    copy_between_volumes(service.volume_name("orderly"), service.volume_name(volume_name), path_to_reports)
 
 
-def add_guidance_to_contrib_portal(service, path_to_reports):
-    print("- Copying burden estimate templates from orderly to contrib portal")
-    copy_between_volumes(service.volume_name("orderly"), service.volume_name("guidance"), path_to_reports)
-
-
-def get_template_report_versions():
-    with open("template_report_versions") as f:
+def get_report_versions(path):
+    with open(path) as f:
         value = f.read()
     return [x.strip() for x in value.split("\n") if x]
 
-
-def get_guidance_report_versions():
-    with open("guidance_report_versions") as f:
-        value = f.read()
-    return [x.strip() for x in value.split("\n") if x]
