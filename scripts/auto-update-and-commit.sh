3d6a0d4edc6cd09ddec0569af822eef292405eec
Martin Eden <martin.s.eden@pm.me>
2018-08-22 17:13:04 +0100

    Delete and create branch every time

--- scripts/auto-update-and-commit.sh
+++ scripts/auto-update-and-commit.sh
@@ -3,5 +3,9 @@ set -ex
 ./src/update_versions_to_latest_master.py
 git status --porcelain && exit 0;
 
+git push --delete origin latest
+git branch -d latest
+git checkout -b latest
+
 git commit -m "Auto: Update versions to latest"
-git push
+git push -u origin
