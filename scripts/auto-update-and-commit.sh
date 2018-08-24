292efbc75b866740e9463835ffcdfe4bc18dab49
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-08-24 09:19:48 +0100

    Revert "Try to avoid rebuilding when not necessary"
    This reverts commit 59a07de065059b7b452181fe4bf9804900d5adba.
    
    Sorry wrong branch :(

--- scripts/auto-update-and-commit.sh
+++ scripts/auto-update-and-commit.sh
@@ -2,21 +2,15 @@
 set -ex
 message=$(./src/update_versions_to_latest_master.py)
 # https://stackoverflow.com/a/3879077/777939
-git diff-index --quiet HEAD -- && echo "No changes detected" && exit 0
-echo "Changes detected relative to origin/master"
-
-# For some reason, the submodules must be added for "git diff --quiet"
-# to behave as expected. But we were going to do that before commit anyway.
-git add submodules
-git diff --quiet origin/latest -- submodules && \
-    echo "origin/latest contains submodule updates already" && exit 0
-echo "Changes detected relative to origin/latest"
-git reset
+git diff-index --quiet HEAD -- && echo "No changes detected" && exit 0;
+echo "Changes detected"
 
 git remote set-url origin git@github.com:vimc/montagu
 git push --delete origin latest || true
 git branch -D latest || true
 git checkout -b latest
+
+git add submodules
 git commit -m "Auto: Update versions to latest
 
 $message"
