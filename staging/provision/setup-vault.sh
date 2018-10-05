a9a72c62af848ffd8c22027bc5b20c3904979c5c
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-10-05 10:23:01 +0100

    Update vault version

--- staging/provision/setup-vault.sh
+++ staging/provision/setup-vault.sh
@@ -7,8 +7,8 @@ else
 	echo "installing vault"
 	sudo apt-get update
 	sudo apt-get install -y unzip
-	vault_zip=vault_0.7.3_linux_amd64.zip
-	wget https://releases.hashicorp.com/vault/0.7.3/$vault_zip
+	vault_zip=vault_0.11.2_linux_amd64.zip
+	wget https://releases.hashicorp.com/vault/0.11.2/$vault_zip
 	unzip $vault_zip
 	chmod 755 vault
 	sudo cp vault /usr/bin/vault
