2191ade9b21c592d5d16aa928a85192efe322a92
Alex <alex.hill@gmail.com>
2019-06-24 15:25:48 +0100

    kill and echo x

--- clear-docker.sh
+++ clear-docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 if [[ ! $1 == "--force" ]]; then
     echo "Warning: This will stop and remove all Docker containers running on "
@@ -10,7 +11,7 @@ if [[ ! $1 == "--force" ]]; then
 fi
 
 echo "Stopping and removing all Docker containers..."
-docker stop $(docker ps -aq)
+docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker volume prune --force
 exit 0
