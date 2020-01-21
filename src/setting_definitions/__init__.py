daf3554e61e9fd1795f5ea05fb92be5dddff6394
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-01-21 09:27:16 +0000

    Added orderly_web_api_url to settings definitions

--- src/setting_definitions/__init__.py
+++ src/setting_definitions/__init__.py
@@ -66,6 +66,14 @@ definitions = [
                       "\n- support.montagu.dide.ic.ac.uk (for stage)"
                       "\n- montagu.vaccineimpact.org (for production)",
                       default_value="localhost"),
+    SettingDefinition("orderly_web_api_url",
+                        "What is the URL of the OrderlyWeb api, used to coordinate adding users to both Montagu and OrderlyWeb? "
+                        "Likely values:"
+                        "\n http://localhost:8888/api/v1 (for local dev)"
+                        "\n https://support.montagu.dide.ic.ac.uk:10443/reports/api/v1 (for UAT)"),
+                        "\n https://support.montagu.dide.ic.ac.uk:11443/reports/api/v1 (for Science)"
+                        "\n https://montagu.vaccineimpact.org/reports/api/v1 (for Production)",
+                        default_value="http://localhost:8888/api/v1"),
     EnumSettingDefinition("certificate",
                           "What SSL certificate should Montagu use?",
                           [
