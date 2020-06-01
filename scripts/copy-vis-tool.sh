3ecb8c5aba8bbb065b4748ff060d2ab1d991a683
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-06-01 13:53:05 +0100

    Script to copy in vis tool

---
+++ scripts/copy-vis-tool.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+
+PROXY_CONTAINER=montagu_proxy_1
+ORDERLY_CONTAINER=orderly_web_orderly
+ORDERLY_REPORT=paper-first-public-app
+ORDERLY_ID=20200115-105913-be6fcf5b
+
+ORDERLY_PATH="/orderly/archive/$ORDERLY_REPORT/$ORDERLY_ID"
+WWW_ROOT=/usr/share/nginx/html
+
+docker exec -it $PROXY_CONTAINER mkdir -p $WWW_ROOT/visualisation
+
+mkdir -p visualisation
+docker cp $ORDERLY_CONTAINER:$ORDERLY_PATH visualisation/2020
+docker cp visualisation/2020 $PROXY_CONTAINER:$WWW_ROOT/visualisation
