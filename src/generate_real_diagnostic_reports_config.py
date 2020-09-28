022703a82b3bd72a4aa38461fea1f9f130a60217
Alex <alex.hill@gmail.com>
2020-09-28 14:38:04 +0100

    regenerate config

--- src/generate_real_diagnostic_reports_config.py
+++ src/generate_real_diagnostic_reports_config.py
@@ -46,7 +46,7 @@ def generate():
         "UND-Moore": ["JE"],
         "UND-Perkins": ["YF"],
         "Yale-Pitzer": ["Typhoid"],
-        "VIMC" ["Tyhoid", "Cholera"]
+        "VIMC": ["Tyhoid", "Cholera"]
     }
 
     group_PIs = {
