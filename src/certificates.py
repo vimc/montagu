dd70294aa8910013507a65f0639cceebb0f9c3f3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-03-27 12:39:29 +0100

    Pull in new certificates etc

--- src/certificates.py
+++ src/certificates.py
@@ -18,6 +18,12 @@ def get_ssl_certificate(certificate_type: str):
         result = production()
     elif certificate_type == "support":
         result = support()
+    elif certificate_type == "science":
+        result = science()
+    elif certificate_type == "uat":
+        result = uat()
+    elif certificate_type == "production2":
+        result = production2()
     elif certificate_type == "self_signed_fresh":
         result = self_signed_fresh()
     elif certificate_type == "self_signed":
@@ -66,6 +72,21 @@ def support():
     return real_certificate("support", "support.montagu.crt", "ssl/v2/support/")
 
 
+def science():
+    print("- Using science certificate (science.montagu.dide.ic.ac.uk)")
+    return real_certificate2("science")
+
+
+def uat():
+    print("- Using uat certificate (uat.montagu.dide.ic.ac.uk)")
+    return real_certificate2("uat")
+
+
+def production2():
+    print("- Using production2 certificate (production2.montagu.dide.ic.ac.uk)")
+    return real_certificate2("production2")
+
+
 def real_certificate(local_folder, local_name, vault_path):
     copy(cert_path(local_folder, local_name), paths.ssl)
 
@@ -77,6 +98,24 @@ def real_certificate(local_folder, local_name, vault_path):
 
     return {
         "certificate": join(paths.ssl, local_name),
+        "key": key_path,
+        "dhparam": dhparam_path
+    }
+
+def real_certificate2(instance_name):
+    vault_path = f"ssl/v2/{instance_name}/"
+
+    cert_path = join(paths.ssl, "ssl.cert")
+    save_secret_to_file(vault_path + "cert", field="value", output=cert_path)
+
+    key_path = join(paths.ssl, "ssl.key")
+    save_secret_to_file(vault_path + "key", field="value", output=key_path)
+
+    dhparam_path = join(paths.ssl, "dhparam.pem")
+    save_secret_to_file(vault_path + "dhparam", field="value", output=dhparam_path)
+
+    return {
+        "certificate": cert_path,
         "key": key_path,
         "dhparam": dhparam_path
     }
